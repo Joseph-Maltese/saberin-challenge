7c71010d4547c42f816820d82f25873ae64a7543
Joseph Maltese <josephmaltese012@gmail.com>
2025-03-11 20:35:05 -0400

    Update 'Add Song' form to require users to enter Title, Artist, Album, and Genre.
    Update Client/Pages/AddSong.razor: add OnInvalidSubmit parameter to EditForm component to fire HandleInvalidSubmit callback. Remove ValidationSummary component (prevents individual field error messages from popping up). Create HandleInvalidSubmit function; called when form submission is invalid, sets 'message' to let users know they entered the form wrong.
    
    Update Shared/Song.cs and music-manager-start.Data/Models/Song.cs: update Song class fields 'Title', 'Artist', 'Album', and 'Genre' to contain 'Required' validation attributes; enforcing non-empty, non-whitespace input for each field in Client/Pages/AddSong.razor

--- music-manager-start.Data/Models/Song.cs
+++ music-manager-start.Data/Models/Song.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,17 @@ namespace music_manager_starter.Data.Models
 {
     public sealed class Song
     {
+        /*(*NEWBYME*) updating Song class fields 'Title', 'Artist', 'Album', and 'Genre' to contain 
+        'Required' validation attributes; to make sure each field gets a value (cannot be whitespace)
+        Help with validation attributes here: https://learn.microsoft.com/en-us/aspnet/core/blazor/forms/validation?view=aspnetcore-9.0 */
         public Guid Id { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Artist { get; set; }
+        [Required]
         public string Album { get; set; }
+        [Required]
         public string Genre { get; set; }
         //(*NEWBYME*) adding field for Art; if the user wishes to upload album art when adding a Song
         public byte[]? Art { get; set; } 
